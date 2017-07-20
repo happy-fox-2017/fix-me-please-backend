f8f08d66cc2545f989f395290689cc17c4d998c6
Antoni Angga <antoniangga14@gmail.com>
2017-07-20 10:15:32 +0700

    tadi salah push, ini baru bener push ke master

--- routes/transactions.js
+++ routes/transactions.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router()
 var transactionController = require('../controller/transactions');
 
-router.get('/', transactionsController.all)
-router.post('/', transactionsController.create)
-router.put('/:id', transactionsController.update)
-router.delete('/:id', transactionsController.delete)
+router.get('/', transactionsController.all())
+router.post('/', transactionsController.create())
+router.put('/:id', transactionsController.update())
+router.delete('/:id', transactionsController.delete())
+
+module.exports = router;
