dfdc64f6a8f3d0c8fc57401961ddffe5fadafcc1
ambodalle <ambodalle.st@gmail.com>
2017-07-20 10:43:42 +0700

    get all book ok, create book ok

--- controllers/books.js
+++ controllers/books.js
@@ -10,12 +10,16 @@ module.exports = {
     })
   },
   create: function(req, res) {
-    var book = new Book(req.body);
+    var book = new Book (req.body)
     book.save(function (err, result) {
+         console.log('ini book result');
+         console.log('result' + result);
       if (err) {
+           console.log('ini error'+err);
         res.send({err: err})
-      }
-      res.send(result)
+    } else {
+        res.send(result)
+   }
     });
   },
   update: function(req, res) {
