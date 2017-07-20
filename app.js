128283d74ca77fdbcb53d3f18ffa45a8fe40d474
Zulfikar Annur Ahmad <zlfikar.aa@gmail.com>
2017-07-20 10:15:41 +0700

    add body parser

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/api-crud-mongoose', (err) => {
+mongoose.connect('mongodb://localhost/api-crud-mongoose', {
+  useMongoClient : true
+} ,(err) => {
   err ? console.log('Can\'t connect to database') : console.log('Database connected')
 });
 
