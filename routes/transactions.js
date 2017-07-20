e83e3ab7944338392fd8b8922c466dad572485f2
AndrewSenewe <sindrew.christ@gmail.com>
2017-07-20 10:28:43 +0700

    error book model dan require dari book model fixed

--- routes/transactions.js
+++ routes/transactions.js
@@ -3,12 +3,8 @@ const router = express.Router()
 var transactionsController = require('../controllers/transactions');
 
 router.get('/', transactionsController.all)
-console.log('masuk1');
 router.post('/', transactionsController.create)
-console.log('masuk2');
 router.put('/:id', transactionsController.update)
-console.log('masuk3');
 router.delete('/:id', transactionsController.delete)
-console.log('masuk4');
 
 module.exports = router;
