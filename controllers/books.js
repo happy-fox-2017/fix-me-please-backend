1c0061ac5cb67e8aef33e2d73308f5e2cf89bc16
AndrewSenewe <sindrew.christ@gmail.com>
2017-07-20 10:37:07 +0700

    update saya ganti req.params.id

--- controllers/books.js
+++ controllers/books.js
@@ -20,7 +20,7 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Book.update({ _id: req.id }, {
+    Book.update({ _id: req.params.id }, {
       $set: req.body
     }, function(err, result) {
       if (err) {
