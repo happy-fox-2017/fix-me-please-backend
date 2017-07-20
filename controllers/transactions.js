684e3b54e3de8d449e6ac73e2f949b2b2cb5e1f9
myyusuf <myyusuf1911@gmail.com>
2017-07-20 10:36:11 +0700

    Fix user CRUD

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -31,9 +31,9 @@ module.exports = {
   delete: function(req, res) {
     Transaction.remove({ _id: req.id }, function (err, result) {
       if (err) {
-        res.send({err: err})
+        res.send({ err });
       }
       res.send(result)
-    }
-  });
+    });
+  },
 }
--- routes/transactions.js
+++ routes/transactions.js
@@ -1,8 +1,11 @@
 const express = require('express');
-const router = express.Router()
-var transactionController = require('../controller/transactions');
 
-router.get('/', transactionsController.all)
-router.post('/', transactionsController.create)
-router.put('/:id', transactionsController.update)
-router.delete('/:id', transactionsController.delete)
+const router = express.Router();
+const transactionsController = require('../controllers/transactions');
+
+router.get('/', transactionsController.all);
+router.post('/', transactionsController.create);
+router.put('/:id', transactionsController.update);
+router.delete('/:id', transactionsController.delete);
+
+module.exports = router;
