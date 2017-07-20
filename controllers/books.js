673ebf985fc7ba30c80e4e042b155051b8cee610
1000pilar <nunua7a8@gmailcom>
2017-07-20 10:28:39 +0700

    books test done

--- controllers/books.js
+++ controllers/books.js
@@ -19,17 +19,31 @@ module.exports = {
     });
   },
   update: function(req, res) {
-    Book.update({ _id: req.id }, {
-      $set: req.body
-    }, function(err, result) {
-      if (err) {
-        res.send({err: err})
+    Book.findOne({_id: req.params.id}, (err, book)=>{
+      if(!err){
+        var bookUpdate = {
+          isbn : req.body.isbn || book.isbn,
+          title: req.body.title || book.title,
+          author: req.body.author || book.author,
+          category: req.body.category || book.category,
+          stock: req.body.stock || book.stock
+        }
+        Book.update({ _id: req.params.id }, {
+          $set: bookUpdate
+        }, {new: true}, function(err, result) {
+          if (err) {
+            res.send({err: err})
+          } else {
+            res.send(result)
+          }
+        });
+      } else {
+        res.send({msg: 'id not found'})
       }
-      res.send(result)
-    });
+    })
   },
   delete: function(req, res) {
-    Book.remove({ _id: req.id }, function (err, result) {
+    Book.remove({ _id: req.params.id }, function (err, result) {
       if (err) {
         res.send({err: err})
       }
